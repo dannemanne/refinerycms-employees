3ecfc114f0c689a8235094ab90706a3d77ff41bb
Daniel Wiklund <daniel@happyrabbit.com>
2014-10-24 12:09:46 +0800

    Added spec for sick leave

--- spec/features/refinery/employees/sick_leaves_spec.rb
+++ spec/features/refinery/employees/sick_leaves_spec.rb
@@ -5,9 +5,12 @@ describe Refinery do
   describe "Employees" do
     describe "sick_leaves" do
       refinery_login_with :refinery_user
+      let(:employee) { FactoryGirl.create(:employee, user: logged_in_user) }
+      before :each do
+        employee
+      end
 
       describe "list of sick leaves" do
-        let(:employee) { FactoryGirl.create(:employee, user: logged_in_user) }
         before do
           FactoryGirl.create(:sick_leave, employee: employee, start_date: '2012-01-31')
           FactoryGirl.create(:sick_leave, employee: employee, start_date: '2012-07-31')
@@ -17,6 +20,20 @@ describe Refinery do
           visit refinery.employees_sick_leaves_path
           page.should have_content("2012-01-31")
           page.should have_content("2012-07-31")
+        end
+      end
+
+      describe "registering sick leaves" do
+        context "when there is no recent sick leave" do
+
+          it "can register sick leave for today" do
+            visit refinery.employees_sick_leaves_path
+            click_button "I am sick today"
+
+            page.should have_content("You have registered Sick Leave today")
+            expect( Refinery::Employees::SickLeave.count ).to eq 1
+          end
+
         end
       end
 
