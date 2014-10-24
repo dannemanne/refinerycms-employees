9e052bf790a885114d8d06b01ae9f5a17fe20465
Daniel Wiklund <daniel@happyrabbit.com>
2014-10-24 09:28:42 +0800

    Updated admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Refinery::Core::Engine.routes.draw do
 
   # Admin routes
   namespace :employees, :path => '' do
-    namespace :admin, :path => Refinery::Core.backend_route do
+    namespace :admin, :path => "#{Refinery::Core.backend_route}/employees" do
       resources :employees, :except => :show do
         collection do
           post :update_positions
